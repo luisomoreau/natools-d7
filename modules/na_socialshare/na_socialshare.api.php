749256b2ace68a86d7adce866f6257f7c551a516
nyl_auster <yann.boisselier@gmail.com>
2013-07-13 13:03:43 +0200

    add document for hook

---
+++ modules/na_socialshare/na_socialshare.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Declare classes used to define widget to na_socialshare module.
+ */
+function hook_na_socialshare_widgets_info() {
+  return array(
+    'na_facebooklike',
+    'na_facebooksend',
+    'na_facebookcomment',
+    'na_googleplus',
+    'na_twittershare',
+    'na_twitterfollow',
+  );
+}
+
