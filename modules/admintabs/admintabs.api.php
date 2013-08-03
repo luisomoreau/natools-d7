22a114ed3e5c53189dcbce06bba73c9bf228237e
nyl_auster <yann.boisselier@gmail.com>
2013-08-03 13:55:57 +0200

    fix various issues

--- modules/admintabs/admintabs.api.php
+++ modules/admintabs/admintabs.api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file tabsapi.php
  *
@@ -22,18 +23,20 @@ function hook_admintabs_info() {
   $tabsets['admin/dashboard'] = array(
 
     // title for this menu item
-    'title' => t('Dashboard'),
+    '#title' => t('Dashboard'),
 
-    // define default tabs. This may change according to user roles.
-    // If you want all role to land on same tab by defaut, set this
-    // as array('all' => 'yourtab').
-    'default tab' => array(
+    // if you want different role to land on different tabs according to their role,
+    // define a 'default tab' and associate a role id to a tab path.
+    // In this example, user with role 4 will be redirected to "tips" page when landing
+    // on "admin/dashboard" page.
+    // Do not define this key if you want all users to land on first tab by default.
+    '#default tab' => array(
       3 => 'overview',
       4 => 'tips',
     ),
 
     // now define all the tabs we need inside this tabset.
-    'tabs' => array(
+    '#tabs' => array(
 
       // here is is minimum required to make appear a tab.
       // remember that these are drupal menu_local_tasks, so you need
@@ -42,13 +45,15 @@ function hook_admintabs_info() {
       // tab overview
       'overview' => array('title' => 'Overview'),
       // tab brand
-      'brands' => array('title' => 'Brands'),
+      'tips' => array('title' => 'Tips'),
 
       // define a sub taks to "brands". here we override type to use
       // MENU_LOCAL_ACTION type.
-      'brands/add-brand' => array(
-        'title' => 'Add a brand',
-        'redirect' => 'node/add/brand',
+      // We use a custom "redirect" key, so that user is redirect to node/add/tip form
+      // from this links. a "destination" parameter will be set in url to bring him back to dashboard.
+      'tips/add-tip' => array(
+        'title' => 'Add a tip',
+        'redirect' => 'node/add/tip',
         'type' => MENU_LOCAL_ACTION,
       ),
     ),
